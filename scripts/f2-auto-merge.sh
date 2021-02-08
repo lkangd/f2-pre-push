e7afa6a50d86a8965b57d8d713ea51f1abe9ba3f
梁康达 <liangkd01@mingyuanyun.com>
2021-02-08 13:14:54 +0800

    feat(f2-auto-merge.sh): 完善 auto-merge shell 脚本

--- scripts/f2-auto-merge.sh
+++ scripts/f2-auto-merge.sh
@@ -6,18 +6,25 @@ branch_name=`git symbolic-ref --short -q HEAD`
 if [[ $branch_name == f2* ]]
 then
   # 拉取最新的 master 分支
-  git pull origin master
-  # 合并 master 到当前 f2
-  merge_master_result=`git merge master`
-  echo 'merge result: '$merge_master_result
-  # 判断合并结果
-  if [[ $merge_master_result == 'Already up to date.'* ]]
+  git fetch origin master:master
+  # 检查当前分支是否合并过最新的 master 分支
+  check_merge_status=`git branch --merge ${branch_name} | grep ' master'`
+  # 是则继续推送
+  if [[ $check_merge_status == *' master'* ]]
   then
-    # 提示合并结果
-    echo 'f2 branch '$branch_name' auto merge success！'
+    echo -e '\033[32m Your current f2 branch is up to date with master. \033[0m'
+    exit 0
   else
-    # 合并异常退出
-    echo 'Your current f2 branch is not up to date, please merge master branch to current branch and push again.'
+    # 否则进行 merge master
+    merge_master_result=`git merge master`
+    if [[ $merge_master_result == *'Updating'* ]]
+    then
+      # 合并正常
+      echo -e '\033[34m Your current f2 branch is up to date with master, please push again.  \033[0m'
+    else
+      # 合并异常
+      echo -e '\033[33m Your current f2 branch is not up to date with master, please merge master branch to current branch and push again. \033[0m'
+    fi
     exit 1
   fi
 fi
